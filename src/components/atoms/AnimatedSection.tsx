02d0840e15505f39308213d565c17eb9b323bc72
Aditya Shah <aditya03714@gmail.com>
2026-01-01 23:52:09 +0530

    feat(animations): enhance interactivity with entry animations

---
+++ src/components/atoms/AnimatedSection.tsx
@@ -0,0 +1,20 @@
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
+import { ReactNode } from 'react';
+
+interface AnimatedSectionProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export const AnimatedSection = ({ children, className }: AnimatedSectionProps) => {
+  const [ref, isIntersecting] = useIntersectionObserver({ threshold: 0.1 });
+
+  return (
+    <div
+      ref={ref}
+      className={`transition-opacity duration-1000 ease-in ${isIntersecting ? 'opacity-100' : 'opacity-0'} ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
